6b9a44928577f60e3f20bfd63af5a968c8faf995
imrooteodoro <adelsonteodoro67@gmail.com>
2025-11-29 16:18:59 -0300

    feat: add optional address prop to TimelineSimpleContent component

--- src/app/shared/components/TimelineSimpleContent/index.tsx
+++ src/app/shared/components/TimelineSimpleContent/index.tsx
@@ -3,16 +3,19 @@ import { StyledContainer } from "./styles";
 interface ITimelineSimpleContentProps {
   title: string;
   content: string;
+  address?: string;
 }
 
 export function TimelineSimpleContent({
   title,
   content,
+  address,  
 }: ITimelineSimpleContentProps) {
   return (
     <StyledContainer>
       <h2>{title}</h2>
       <p>{content}</p>
+      {address && <p style={{ color: "gray" }}>Local: {address}</p>}
     </StyledContainer>
   );
 }

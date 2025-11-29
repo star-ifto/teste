c7ff7e2a98fdedbfc35de7a27ac3eb3a03b415de
imrooteodoro <adelsonteodoro67@gmail.com>
2025-11-29 16:18:49 -0300

    feat: make about_name, vacancy, address, twoSpeakers, and course props optional in TimelineContentWithImage component

--- src/app/shared/components/TimelineContentWithImage/index.tsx
+++ src/app/shared/components/TimelineContentWithImage/index.tsx
@@ -12,10 +12,14 @@ interface ITimelineContentWithImageProps {
   title: string;
   image: string;
   name: string;
-  about_name: string;
+  about_name?: string;
   description: string;
+  vacancy?: string;
+  address?: string;
   right?: boolean;
   womanSpeaker?: boolean;
+  twoSpeakers?: boolean;
+  course?: boolean;
 }
 
 export function TimelineContentWithImage({
@@ -24,8 +28,12 @@ export function TimelineContentWithImage({
   name,
   about_name,
   description,
+  vacancy,
   right = false,
+  course = false,
   womanSpeaker = false,
+  address,
+  twoSpeakers = false,
 }: ITimelineContentWithImageProps) {
   const [imageError, setImageError] = useState(false);
 
@@ -36,7 +44,7 @@ export function TimelineContentWithImage({
 
   return (
     <StyledContainer>
-      <h1>Palestra</h1>
+      <h1>{course ? "Minicurso" : "Palestra"}</h1>
       <h2>{title}</h2>
       <StyledContentContainer right={right}>
         <StyledSpeakerInfo right={right}>
@@ -46,15 +54,31 @@ export function TimelineContentWithImage({
               alt={name}
               onError={() => setImageError(true)}
             />
+            {twoSpeakers && (
+              <img
+                src={defaultAvatarImage}
+                alt="Segundo Palestrante"
+                className="second-speaker-image"
+              />
+            )}
           </StyledImageContainer>
 
           <strong>{name}</strong>
         </StyledSpeakerInfo>
 
         <span style={{ color: "blue" }}>{about_name}</span>
+        <span style={{ color: "gray" }}>Local: {address}</span>
+        <span style={{ color: "purple", padding: "0 5px" }}>{vacancy}</span>
 
         <p>{description}</p>
+        <style>
+          {`{
+              padding-top: 10px;
+              text-align: justify;
+            }
+          `}
+        </style>
       </StyledContentContainer>
-    </StyledContainer>
+    </StyledContainer >
   );
 }
